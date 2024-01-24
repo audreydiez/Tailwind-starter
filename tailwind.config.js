bc2719a617ae0773cdea0f2d1a13d346dcd9ebd2
Audrey Diez <audrey.diez@gmail.com>
2024-01-24 15:47:14 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./*.{html,js}'],
+	theme: {
+		extend: {
+			spacing: {
+				128: '8rem',
+				144: '36rem'
+			}
+		}
+	},
+	plugins: []
+}
